02608109a24943f7a4a51faa126dbda00f918e66
DanielRojas1920 <danielrojvilla@gmail.com>
2024-03-16 19:52:08 -0600

    Ejercicio Directivas de Angular

---
+++ src/app/components/lista.component/lista.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+    selector: 'Lista-component',
+    templateUrl: 'lista.component.html',
+})
+
+
+export class ListaComponent implements OnInit{
+
+    constructor() {}
+
+    ngOnInit(){
+        
+    }
+
+    verdadero: boolean = true;
+
+}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
   constructor() {}
 
+  title: string = 'Mi nueva página!';
+
+  hazmeClic(): void {
+    alert("hiciste clic!");
+
+  }
+
+
+
+  verdadero: boolean=true;
+
+  imgUrl: string = "https://i.redd.it/7ksiaa4exhh01.gif";
+
 }
+
+
+
