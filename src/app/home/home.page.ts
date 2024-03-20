859679a4754055c9e5e7e578082259f3af289f07
DanielRojas1920 <danielrojvilla@gmail.com>
2024-03-19 21:17:43 -0600

    Ejercicio 2 de Componente: Ciclo de Vida de un Componente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,42 @@ import { Component} from '@angular/core';
 
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    console.log("AppComponent: Constructor");
+  }
+
+  ngOnChanges() {
+    console.log("AppComponent : OnChanges");
+    
+  }
+
+  ngOnInit(){
+    console.log("AppComponent : OnInit");
+  }
+
+  ngDoCheck (){
+    console.log("AppComponent : DoCheck");
+  }
+
+  ngAfterContentInit(){
+    console.log("AppComponent : AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AppComponent : AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AppComponent : AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AppComponent : AfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("AppComponent : OnDestroy");
+  }
 
   title: string = 'Mi nueva página!';
 
@@ -23,6 +58,9 @@ export class HomePage {
   verdadero: boolean=true;
 
   imgUrl: string = "https://i.redd.it/7ksiaa4exhh01.gif";
+
+
+  nombreAlumno: string = '';
 
 }
 
